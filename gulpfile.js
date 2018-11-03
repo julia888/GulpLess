7d73add8c34b1bd9cbe3e724403ad448ea4f51da
julia888 <juliazasipko@gmail.com>
2018-11-03 18:20:28 +0200

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+const gulp = require('gulp');
+const less = require('gulp-less');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('less', function () {
+    return gulp.src('src/**/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat('all.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('newStyle'));
+});
